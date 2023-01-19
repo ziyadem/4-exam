30e24a4e5ec1a2840a5366a445f6cf94a15f826a
ziyadem <maxkamovaziyaem17@gmail.com>
2023-01-19 21:38:18 +0500

    page

--- js/app.js
+++ js/app.js
@@ -1,8 +1,35 @@
 console.log(123);
-let load=document.querySelector(".load");
-console.log(load);
+let load=document.querySelector(".load"),
+    elForm=document.querySelector("form"),
+    elInputEmail=document.querySelector(".email"),
+    elInputPassword=document.querySelector(".password");
+
+console.log(load,elForm,elInputEmail,elInputPassword);
 
 setTimeout(() => {
     load.style.display="none";
-    console.log(34);
-}, 3000);
+}, 1000);
+
+elForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+
+    fetch("https://reqres.in/api/login",{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify({
+            email:elInputEmail.value,
+            password:elInputPassword.value,
+        }),
+    })
+    .then((res)=>res.json())
+    .then((data)=>{
+        if(data.token){
+            localStorage.setItem('token',data.token);
+            window.location.replace('./index.html');
+        }
+    })
+    .catch((err)=>console.log(err));
+});
+
